7466a9371712dbfd34649a663f3581848cefae82
Kenan <bary.berdiyev.97@mail.ru>
2023-05-30 15:24:10 +0700

    главная страница

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,58 @@
+import cn from 'classnames';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './App.module.scss';
 import { Button, Card } from './components';
 import LinksForm from './components/LinksForm/LinksForm';
+import Modal from './components/Modal/Modal';
+import { useAppSelector } from './hooks/redux';
 import { withLayout } from './layout/Layout';
 
 const App = () => {
+	const [modalActive, setModalActive] = useState<boolean>(false);
+	const { status, error, downloadUrl, previewUrl } = useAppSelector(
+		state => state.google
+	);
+
 	return (
 		<div className={styles.wrapper}>
 			<Card className={styles.card} color='blue'>
 				<LinksForm />
+				{status == 'loading' ? (
+					<span className={cn(styles.info, styles.loading)}>Загрузка...</span>
+				) : (
+					<></>
+				)}
+				{error ? (
+					<span className={cn(styles.info, styles.error)}>{error}</span>
+				) : (
+					<></>
+				)}
 				<div className={styles.buttons}>
-					<Button appearance='ghost'>Пред просмотр</Button>
-					<Button appearance='ghost'>Скачать документ</Button>
+					<Button
+						appearance='ghost'
+						onClick={() => setModalActive(true)}
+						disabled={!previewUrl}
+					>
+						Пред просмотр
+					</Button>
+					<Button appearance='ghost' disabled={!downloadUrl}>
+						<Link
+							to={downloadUrl}
+							className={cn(styles.link, {
+								[styles.disabled]: downloadUrl == ''
+							})}
+						>
+							Скачать документ
+						</Link>
+					</Button>
 				</div>
 			</Card>
+			<Modal
+				active={modalActive}
+				setActive={setModalActive}
+				previewUrl={previewUrl}
+			/>
 		</div>
 	);
 };
